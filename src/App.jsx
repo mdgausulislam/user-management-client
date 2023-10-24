0fbb952194ba4ed3e8ee71fb490a2c141eff3eb8
unknown <gausulislam5@gmail.com>
2023-10-25 01:16:02 +0600

    update client site data sent server site

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,46 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [user,setUser]=useState([]);
-  useEffect(()=>{
+  const [user, setUser] = useState([]);
+  useEffect(() => {
     fetch('http://localhost:5000/user')
-    .then(res=>res.json())
-    .then(data=>setUser(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, []);
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const userForm = event.target;
+    const name=userForm.name.value;
+    const email=userForm.email.value;
+    const user={name,email}
+    fetch('http://localhost:5000/user',{
+      method:'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify(user);
+    })
+
+    console.log(name,email);
+  }
 
   return (
     <>
       <h1>User Management System</h1>
       <h3>Numbers of users: {user.length}</h3>
 
-      <form>
+      <form onSubmit={handleAddUser}>
         <input type="text" name="name" id="" />
-        <br/>
+        <br />
         <input type="email" name="email" id="" />
-        <br/>
+        <br />
         <input type="submit" value="Submit" />
       </form>
 
       <div>
         {
-          user.map(u=><p key={u.id}>{u.id}: {u.name}: {u.email}</p>)
+          user.map(u => <p key={u.id}>{u.id}: {u.name}: {u.email}</p>)
         }
       </div>
     </>
